2903255c4e0a83107f301169135f05723cbe8eee
rajat-gith <rajatnayak1582002@gmail.com>
2023-08-30 01:02:09 +0530

    Data changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
-import { useEffect, useState } from "react";
-import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+import mixpanel from "mixpanel-browser";
+
+mixpanel.init("208345f230d8cd80b5f6791ad1d7a1d5", { debug: true });
 
 function App() {
   return (

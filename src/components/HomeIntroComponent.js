c89d4dc50a5c9e666332536ab45ba85a6cb0ccf7
rajat-gith <rajatnayak1582002@gmail.com>
2024-07-07 14:13:18 +0530

    ui changes

--- src/components/HomeIntroComponent.js
+++ src/components/HomeIntroComponent.js
@@ -5,32 +5,15 @@ import { motion } from "framer-motion";
 import { TypeAnimation } from "react-type-animation";
 import { AppContext } from "../context/AppContext";
 import { Link } from "react-router-dom";
+import InfoComponent from "./InfoComponent";
 
 function HomeIntroComponent() {
-  const { startShow, setStart } = useContext(AppContext);
-
-  useEffect(() => {}, [startShow]);
   return (
-    <motion.div
-      initial={{ scale: 10 }}
-      animate={{ scale: 1 }}
-      transition={{ duration: 1.5, type: "spring" }}
-      onAnimationComplete={setStart}
-      className="HomeIntroComponent"
-    >
+    <div className="HomeIntroComponent">
       <p>
         Hii Its <span>Rajat</span>
       </p>
-      {startShow ? (
-        <motion.div
-          initial={{ scale: 0 }}
-          animate={{ scale: 1 }}
-          transition={{ type: "spring", duration: "1.5" }}
-          className="imageDiv"
-        >
-          <img src={Photo} />
-        </motion.div>
-      ) : null}
+      <InfoComponent/>
       <Link to="/blogs">
         <button class="blog-button">
           Explore Blogs
@@ -43,32 +26,7 @@ function HomeIntroComponent() {
           </svg>
         </button>
       </Link>
-
-      {startShow ? (
-        <motion.div
-          initial={{ scale: 0 }}
-          animate={{ scale: 1 }}
-          transition={{ type: "spring", duration: "1.5" }}
-          className="imageDiv"
-        >
-          <TypeAnimation
-            sequence={[
-              "A Web Developer",
-              1000,
-              "An Android Developer",
-              1000,
-              "An UI & UX Designer",
-              1000,
-              "A Robotics Enthusiast",
-              1000,
-            ]}
-            wrapper="span"
-            speed={50}
-            repeat={Infinity}
-          />
-        </motion.div>
-      ) : null}
-    </motion.div>
+    </div>
   );
 }
 
---
+++ src/components/InfoComponent.js
@@ -0,0 +1,91 @@
+import React from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+function InfoComponent() {
+  const animationVariants = {
+    hidden: { y: 200, opacity: 0 },
+    visible: (i) => ({
+      y: 0,
+      opacity: 1,
+      transition: {
+        delay: i * 0.4,
+        duration: 1.8,
+        ease: "easeOut",
+      },
+    }),
+  };
+
+  return (
+    <AppContainer>
+      <Wrapper>
+        <Line className="singleLine">
+          <Text>
+            D<span>E</span>V<span>E</span>L<span>O</span>PER,
+          </Text>
+        </Line>
+        <Line className="singleLine">
+          <Text>
+            D<span>E</span>SIG<span>N</span>ER,
+          </Text>
+        </Line>
+        <Line className="singleLine">
+          <Text>
+            <span>R</span>EADER,
+          </Text>
+        </Line>
+      </Wrapper>
+    </AppContainer>
+  );
+}
+
+export default InfoComponent;
+
+const AppContainer = styled.div`
+  width: 50vw;
+  height: 50vh;
+  color: #000000;
+  background: #1d1d1f;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Line = styled.div`
+  width: 100%;
+  height: 7vw;
+  position: relative;
+  overflow: hidden;
+
+  &:nth-of-type(1) {
+    display: flex;
+    justify-content: flex-end;
+  }
+  &:nth-of-type(3) {
+    display: flex;
+    justify-content: center;
+  }
+`;
+
+const Text = styled(motion.div)`
+  position: absolute;
+  font-size: 8vw;
+  color: #fff;
+  line-height: 8vw;
+
+  span {
+    font-family: "Major Mono Display", monospace;
+    color: rgb(150, 149, 149);
+    font-size: 8vw;
+  }
+`;
